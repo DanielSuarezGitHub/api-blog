c2d8412bd083045d8c2373036eee16b7636ad736
Daniel Suarez <danielsuarezscripts@gmail.com>
2023-04-29 10:27:16 -0700

    small changes

--- index.js
+++ index.js
@@ -1,76 +1,41 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const passport = require("passport");
-const { Strategy: LocalStrategy } = require("passport-local");
-const { Strategy: JWTStrategy, ExtractJwt } = require("passport-jwt");
-const { UserModel: User } = require("./models/userSchema");
-const user = require('./routes/userRouter')
-const bcrypt = require("bcryptjs");
 const authRouter = require("./routes/AuthRouter");
-const dotenv = require("dotenv");
-dotenv.config();
+const cors = require('cors')
+const passport = require('passport')
+require('./passport')
+
 
 const port = process.env.PORT;
 const MONGODBURL = "mongodb://mongo:CqSwT4VM97ssiFKbDwe1@containers-us-west-90.railway.app:5951";
 const app = express();
-
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(MONGODBURL);
 }
-
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await User.findOne({ username: username });
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-      bcrypt.compare(password, user.password, (err, res) => {
-        if (res) {
-          // passwords match! log user in
-          return done(null, user);
-        } else {
-          // passwords do not match!
-          return done(null, false, { message: "Incorrect password" });
-        }
-      });
-    } catch (err) {
-      return done(err);
-    }
-  })
-);
-
-passport.use(
-  new JWTStrategy(
-    {
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET_KEY,
-    },
-    (jwtPayload, cb) => {
-      User.findById(jwtPayload.id)
-        .then((user) => {
-          if (!user) {
-            return cb(null, false);
-          }
-          return cb(null, user);
-        })
-        .catch((err) => {
-          return cb(err, false);
-        });
-    }
-  )
-);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.json({ hello: "hello" });
+app.use(cors())
+app.use((req, res, next) => {
+  console.log(req.headers.authorization);
+  console.log(process.env.JWT_SECRET_KEY)
+  console.log('Working')
+  next();
 });
 
-app.use("/api", authRouter);
-app.use('/api', passport.authenticate('jwt', {session: false}), user);
+
+app.use("/", authRouter);
+
+
+
+app.use('/profile', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+  res.send(req.user)
+});
+
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
+
+
+module.exports = app;
