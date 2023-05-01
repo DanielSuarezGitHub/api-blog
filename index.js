4018b392e2196b2f55f7f52c50b05b27808b7a85
Daniel Suarez <danielsuarezscripts@gmail.com>
2023-04-30 18:21:05 -0700

    Remove excess code

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRouter = require("./routes/AuthRouter");
+const postRouter = require("./routes/PostRouter")
 const cors = require('cors')
-const passport = require('passport')
 require('./passport')
 
 
 const port = process.env.PORT;
-const MONGODBURL = "mongodb://mongo:CqSwT4VM97ssiFKbDwe1@containers-us-west-90.railway.app:5951";
+const MONGODB_URL = "mongodb://mongo:CqSwT4VM97ssiFKbDwe1@containers-us-west-90.railway.app:5951";
 const app = express();
 main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect(MONGODBURL);
+  await mongoose.connect(MONGODB_URL);
 }
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,12 +25,8 @@ app.use((req, res, next) => {
 
 
 app.use("/", authRouter);
+app.use("/", postRouter)
 
-
-
-app.use('/profile', passport.authenticate('jwt', {session: false}), (req, res, next) => {
-  res.send(req.user)
-});
 
 
 app.listen(port, () => {
